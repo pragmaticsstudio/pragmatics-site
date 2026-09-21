6b13dd82924b04fc93dab52ff3b401c5fde2cb9e
Charlie Gedeon <charlesgedeon@users.noreply.github.com>
2026-09-21 08:34:08 -0400

    Fix GitHub Pages fallback deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://www.pragmatics.studio',
+  site: 'https://pragmaticsstudio.github.io/pragmatics-site',
   build: { format: 'directory' },
 });
